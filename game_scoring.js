4184b5ae35184de41c3d0c669b651a99391f7a97
Davitpurba <113511758+Davitpurba@users.noreply.github.com>
2026-02-25 10:23:55 +0700

    Create game_scoring.js

---
+++ game_scoring.js
@@ -0,0 +1,42 @@
+// Davit Saputra Purba
+
+
+function calculateScore(input) {
+  let score = 0;
+  let winStreak = 0;
+
+  for (const char of input) {
+    if (!['W', 'D', 'L'].includes(char)) {
+      return 'INVALID';
+    }
+
+    if (char === 'W') {
+      score += 3;
+      winStreak++;
+
+      if (winStreak % 3 === 0) {
+        score += 5;
+      }
+    } else if (char === 'D') {
+      score += 1;
+      winStreak = 0;
+    } else if (char === 'L') {
+      score += 0;
+      winStreak = 0;
+    }
+  }
+
+  return score;
+}
+
+const inputs = [
+  "WDLWWDL",
+  "LDLWWWLDWWW",
+  "DLWWWWLLDWD",
+  "WWWLDDLW",
+  "WALD"
+];
+
+inputs.forEach(i => {
+  console.log(calculateScore(i));
+});
